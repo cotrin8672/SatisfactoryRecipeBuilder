d5926482df8cbd214092c88c2f009441f73e4952
cotrin8672 <gummy8672@gmail.com>
2024-10-31 19:37:13 +0900

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/io/github/cotrin8672/App.kt
@@ -0,0 +1,13 @@
+package io.github.cotrin8672
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+
+    }
+}
